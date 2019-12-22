92fc4041e0d9f1daa0a4b7dafd0b2972a3cea781
EC2 Default User <ec2-user@ip-172-31-32-163.us-east-2.compute.internal>
2019-12-22 10:49:48 +0000

    tmtc

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,5 +2,11 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+    if message.text.include? "Bonjour"
+        message.reply(text: "wesh magl")
+    elsif message.text.include? "Bonne nuit"
+    message.reply(text: "dort bien bitch")
+    else
+        message.reply(text: "Tu casse les couilles")
+    end
 end
